cd35da63d9b1d6d98eb26488f1a676f6e6e2d83d
Marie <marie@Maries-MacBook-Pro.local>
2018-10-05 21:18:34 +0200

    clean up

--- src/modules/BottleBtn.js
+++ src/modules/BottleBtn.js
@@ -2,14 +2,7 @@ import React, { Component } from "react";
 import "../css/BottleBtn.css";
 
 class BottleBtn extends Component {
-  //   state = { up: true };
-
-  //   togglePosition = () => {
-  //     this.setState({ up: !this.state.up });
-  //   };
-
   render() {
-    // const { up } = this.state;
     const { up, image, left, right, onClick } = this.props;
 
     const downClass = left
@@ -18,11 +11,7 @@ class BottleBtn extends Component {
 
     return (
       <div className="bottleBtn" onClick={onClick}>
-        <img
-          src={image}
-          className={up ? "bottleImgUp" : downClass}
-          //   onClick={this.togglePosition}
-        />
+        <img src={image} className={up ? "bottleImgUp" : downClass} />
       </div>
     );
   }
