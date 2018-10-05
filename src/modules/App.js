5ffe0659ba9c3405be5c71f5d77d325cb9a19b03
Marie <marie@Maries-MacBook-Pro.local>
2018-10-05 10:33:53 +0200

    add Header

--- src/modules/App.js
+++ src/modules/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from "react";
 import "../css/App.css";
 
+import Header from "./Header";
+
 class App extends Component {
   render() {
-    return <div className="App">Zelenka</div>;
+    return (
+      <div className="container">
+        <Header />
+      </div>
+    );
   }
 }
 
---
+++ src/modules/Header.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import "../css/Header.css";
+
+import logo from "../images/logo.png";
+import title_img from "../images/title-img.png";
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="header">
+        <img src={title_img} className="title-img" />
+        <div className="logo-container">
+          <p className="logo-text">Программа о хирургии и медицине</p>
+          <img src={logo} className="logo" />
+          <p className="logo-text">
+            Рассказываем и показываем так, чтобы не тошнило
+          </p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Header;
