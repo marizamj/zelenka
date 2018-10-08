f61cc40ff053e7d7498fcc559c27ee8a38815fa2
Marie <marie@Maries-MacBook-Pro.local>
2018-10-08 12:55:00 +0200

    Change font to PT Sans

---
+++ src/modules/Partners.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "../css/Partners.css";
+
+class Partners extends Component {
+  render() {
+    return <div className="partners">bla</div>;
+  }
+}
+
+export default Partners;
