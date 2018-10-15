f22651e8abd2d011e30d6dc6f417bfb6b4cdfca5
Marie <marie@Maries-MacBook-Pro.local>
2018-10-15 13:09:35 +0200

    add title to the video

--- src/modules/Video.js
+++ src/modules/Video.js
@@ -5,8 +5,10 @@ class Video extends Component {
   render() {
     return (
       <div className="video">
+        <h2>Первый выпуск</h2>
         <iframe
           className="iframe"
+          title="First video"
           src="https://www.youtube.com/embed/rQvIR1oL1vE"
           frameBorder="0"
           allow="autoplay; encrypted-media"
