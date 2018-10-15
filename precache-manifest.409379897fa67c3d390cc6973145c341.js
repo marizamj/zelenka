1b78da2eeba7df596ac3e633d0bc3af2d9669fb6
Marie <marie@Maries-MacBook-Pro.local>
2018-10-15 17:51:43 +0200

    Updates

---
+++ precache-manifest.409379897fa67c3d390cc6973145c341.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "9527ccf5f1dfd5aa754a50cfa2e59ed2",
+    "url": "/static/media/youtube.9527ccf5.png"
+  },
+  {
+    "revision": "4687d668504486560f0a",
+    "url": "/static/css/main.8c984f75.chunk.css"
+  },
+  {
+    "revision": "6af62e74a8bccccb03ac",
+    "url": "/static/js/1.6af62e74.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f4c0d88ffbc56154cc962a7d8068f817",
+    "url": "/static/media/logo.f4c0d88f.png"
+  },
+  {
+    "revision": "55c23b4deb48cc1ad2327b85d910fbdc",
+    "url": "/static/media/facebook.55c23b4d.png"
+  },
+  {
+    "revision": "4687d668504486560f0a",
+    "url": "/static/js/main.4687d668.chunk.js"
+  },
+  {
+    "revision": "8e469ae877459bd6efd5bdbc0f6da36b",
+    "url": "/static/media/logo-nlv.8e469ae8.jpg"
+  },
+  {
+    "revision": "9c103c454b279121db7d3d2ff4a49cf0",
+    "url": "/static/media/btn-partners.9c103c45.png"
+  },
+  {
+    "revision": "36201636c580e73d299862a9fdd88b32",
+    "url": "/static/media/btn-all.36201636.png"
+  },
+  {
+    "revision": "f8197bfea0f834389061bd372c24cad4",
+    "url": "/static/media/top-bottle.f8197bfe.png"
+  },
+  {
+    "revision": "59b8ce0407222239b4c9670cec4cf66b",
+    "url": "/static/media/sm-back.59b8ce04.png"
+  },
+  {
+    "revision": "ab0456b45a8c188f2189a7faa25ab8c6",
+    "url": "/index.html"
+  }
+];
--- src/lib/globals.js
+++
@@ -1,2 +0,0 @@
-export const HOME_URL = "http://192.168.2.51:3000/";
-export const HOME_EMAIL = "marizamj@gmail.com";
