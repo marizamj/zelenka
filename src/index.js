7b475346be8716e04a4cee531bdf0a327bb3c53f
Marie <marie@Maries-MacBook-Pro.local>
2018-10-15 17:22:51 +0200

    implement react-router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import "./css/index.css";
 import App from "./modules/App";
+import Form from "./modules/Form";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <BrowserRouter>
+    <div>
+      <Route exact path="/" component={App} />
+      <Route path="/form-partners" component={Form} />
+      <Route path="/form-all" component={Form} />
+      <Route path="/form-success" component={Form} />
+    </div>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
