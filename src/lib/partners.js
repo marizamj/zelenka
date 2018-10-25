8f5ad72756a490191b51ad8b3a4b6b9b35764a2a
Marie Zamzhitskaya <marizamj@gmail.com>
2018-10-25 22:37:54 +0200

    refactor Partners.js

---
+++ src/lib/partners.js
@@ -0,0 +1,25 @@
+import nlv from "../images/logo-nlv.png";
+import pv from "../images/logo-pv.png";
+import afisha from "../images/logo-afisha.png";
+
+export const partners = [
+  {
+    id: "pv",
+    title: "Бар «Петров и  Васечка»",
+    url: "http://piv.pub/",
+    img: pv
+  },
+  {
+    id: "nlv",
+    title: "Клиника лазерной коррекции зрения «Новый взгляд»",
+    url: "http://www.nlv.ru/",
+    img: nlv
+  },
+  {
+    id: "afisha",
+    title: "Материал «Афиша-Daily» о нас",
+    url:
+      "https://daily.afisha.ru/news/20920-vyshel-pervyy-vypusk-bloga-zelenka-tam-rasskazyvayut-o-hirurgii-tak-chtoby-ne-toshnilo/",
+    img: afisha
+  }
+];
--- src/modules/Partners.js
+++ src/modules/Partners.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "../css/Partners.css";
 
-import nlv from "../images/logo-nlv.png";
+import { partners } from "../lib/partners.js";
 
 class Partners extends Component {
   state = { visibleName: null };
@@ -12,28 +12,27 @@ class Partners extends Component {
     return (
       <div className="partners">
         <h2>Уже работают с нами</h2>
-        <div className="partner-container">
-          <span
-            className={`partner-name ${
-              visibleName === "nlv" ? "opacity1" : "opacity0"
-            }`}
-          >
-            Клиника лазерной коррекции зрения «Новый взгляд»
-          </span>
-          <a
-            href="http://www.nlv.ru/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src={nlv}
-              alt="Клиника лазерной коррекции зрения «Новый взгляд»"
-              className="partner-logo"
-              onMouseEnter={() => this.setState({ visibleName: "nlv" })}
-              onMouseLeave={() => this.setState({ visibleName: null })}
-            />
-          </a>
-        </div>
+
+        {partners.map(partner => (
+          <div className="partner-container" key={partner.id}>
+            <span
+              className={`partner-name ${
+                visibleName === partner.id ? "opacity1" : "opacity0"
+              }`}
+            >
+              {partner.title}
+            </span>
+            <a href={partner.url} target="_blank" rel="noopener noreferrer">
+              <img
+                src={partner.img}
+                alt={partner.title}
+                className="partner-logo"
+                onMouseEnter={() => this.setState({ visibleName: partner.id })}
+                onMouseLeave={() => this.setState({ visibleName: null })}
+              />
+            </a>
+          </div>
+        ))}
       </div>
     );
   }
