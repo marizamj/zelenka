d68abfbe39224c8a054e4e644dcea12773ccce73
Maria Zamzhitskaya <marizamj@gmail.com>
2020-03-08 00:18:32 +0200

    Updates

---
+++ precache-manifest.72401b5170a397d9eec057767d30b49f.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "004ba3f3dbacb0314d62722ff26a7563",
+    "url": "/index.html"
+  },
+  {
+    "revision": "283c3d037b6615db3e51",
+    "url": "/static/css/main.69e7b858.chunk.css"
+  },
+  {
+    "revision": "d290b6d32221dae4de9f",
+    "url": "/static/js/2.679dedc2.chunk.js"
+  },
+  {
+    "revision": "b2490fbb34248c3403116f401942d851",
+    "url": "/static/js/2.679dedc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "283c3d037b6615db3e51",
+    "url": "/static/js/main.4eedbde9.chunk.js"
+  },
+  {
+    "revision": "f790417304f24a1448db",
+    "url": "/static/js/runtime-main.5ba7a74e.js"
+  },
+  {
+    "revision": "82397f7e3cda23e956a7d0098603468c",
+    "url": "/static/media/btn-all.82397f7e.png"
+  },
+  {
+    "revision": "8fb554580ddb9497bd95930f4c26ffff",
+    "url": "/static/media/btn-partners.8fb55458.png"
+  },
+  {
+    "revision": "d703c6bc73e8702c44255c5a214d3c4c",
+    "url": "/static/media/facebook.d703c6bc.png"
+  },
+  {
+    "revision": "cc5a1e66dea060c682e970077ace8106",
+    "url": "/static/media/logo-nlv.cc5a1e66.png"
+  },
+  {
+    "revision": "bfd8886b4caada899a1fa2508008b779",
+    "url": "/static/media/logo-pv.bfd8886b.png"
+  },
+  {
+    "revision": "f4c0d88ffbc56154cc962a7d8068f817",
+    "url": "/static/media/logo.f4c0d88f.png"
+  },
+  {
+    "revision": "3de9b9c5bce7e510f056949ae234c9d5",
+    "url": "/static/media/sm-back.3de9b9c5.png"
+  },
+  {
+    "revision": "5a1b5fb6bbf35c053ed85c67b59da4fb",
+    "url": "/static/media/thank-you.5a1b5fb6.png"
+  },
+  {
+    "revision": "5bc95b3cd59e6e60a2560dabcfb33824",
+    "url": "/static/media/top-bottle.5bc95b3c.png"
+  },
+  {
+    "revision": "f5633224819ea1c7ba06a69eb3d48397",
+    "url": "/static/media/youtube.f5633224.png"
+  }
+]);
